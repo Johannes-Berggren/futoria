37ec74138ef809c62191d7330a6e67b604921a5c
Johannes Berggren <jb@learnlink.no>
2022-07-08 21:38:02 +0200

    Install Nuxt.

---
+++ app.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-container>
+    <v-row align="center" style="height: 80vh" class="text-center">
+      <v-col>
+        <v-img src="/futoria-logo.png" />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<style lang="sass">
+body
+  background-color: #000f42
+</style>
